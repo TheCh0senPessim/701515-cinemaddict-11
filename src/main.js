8fc16d4cb6cba413cc78e6cf1fe4f2c0f3c1ce74
Никита <klosr@list.ru>
2020-04-07 02:23:18 +0300

    мелкие исправления

--- src/main.js
+++ src/main.js
@@ -266,29 +266,31 @@ const createMovieDetailsPopupTemplate = () => {
 
 // Функция для рендеринга
 
-const render = (container, template, place) => {
+const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
 };
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = document.querySelector(`.header`);
 
-render(siteHeaderElement, createUserRankTemplate(), `beforeend`);
-render(siteMainElement, createSiteMenuTemplate(), `beforeend`);
-render(siteMainElement, createCatalogTemplate(), `beforeend`);
+render(siteHeaderElement, createUserRankTemplate());
+render(siteMainElement, createSiteMenuTemplate());
+render(siteMainElement, createCatalogTemplate());
 
 const moviesList = siteMainElement.querySelector(`.films-list`);
 const moviesListAll = siteMainElement.querySelector(`.films-list__container`);
 const moviesListExtra = siteMainElement.querySelectorAll(`.films-list--extra`);
 
 for (let i = 0; i < MOVIES_COUNT; i++) {
-  render(moviesListAll, createMovieCardTemplate(), `beforeend`);
+  render(moviesListAll, createMovieCardTemplate(),);
 }
-render(moviesList, createShowMoreButtonTemplate(), `beforeend`);
+render(moviesList, createShowMoreButtonTemplate(),);
 
 for (let i = 0; i < moviesListExtra.length; i++) {
   let extraFilmsContainer = moviesListExtra[i].querySelector(`.films-list__container`)
   for (let j = 0; j < MOVIES_EXTRA_COUNT; j++) {
-    render(extraFilmsContainer, createMovieCardTemplate(), `beforeend`);
+    render(extraFilmsContainer, createMovieCardTemplate(),);
   }
 }
+
+render(siteMainElement, createMovieDetailsPopupTemplate())
